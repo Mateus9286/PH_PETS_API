66925fb725d0b2797e16fb18fc15b72e5982f3e7
Mateus9286 <106389123+Mateus9286@users.noreply.github.com>
2023-09-06 11:34:18 -0300

    fix:ts

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
-// build.sh
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 yarn
-yarn typeorm migration:generate src/migrations/initial -d src/data-source.ts
-yarn typeorm migration:run -d src/data-source
+yarn build
+yarn typeorm migration:run -d dist/data-source
--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,10 @@ import { handleErrors } from "./errors";
 import "reflect-metadata";
 import "dotenv/config";
 import { loginRoutes, productsRoutes, userRoutes } from "./routers";
+import cors from "cors";
 
 const app: Application = express();
+app.use(cors());
 app.use(express.json());
 app.use(handleErrors);
 app.use(userRoutes);
